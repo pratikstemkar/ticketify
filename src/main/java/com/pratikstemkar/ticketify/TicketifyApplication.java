8664fe6580a8337a26ed37afd01e4dc47a0ee608
pratikstemkar <pratikstemkar@gmail.com>
2024-03-10 10:41:08 +0530

    spring boot init

---
+++ src/main/java/com/pratikstemkar/ticketify/TicketifyApplication.java
@@ -0,0 +1,13 @@
+package com.pratikstemkar.ticketify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketifyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pratikstemkar/ticketify/TicketifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pratikstemkar.ticketify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
