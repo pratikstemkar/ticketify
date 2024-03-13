8976f8fe3105b61b90631ee5db095b28d4e5740d
pratikstemkar <pratikstemkar@gmail.com>
2024-03-13 21:21:52 +0530

    seo

--- web/app/not-found.tsx
+++ web/app/not-found.tsx
@@ -1,5 +1,16 @@
-const NotFound = () => {
-    return <div>NotFound</div>;
-};
+import Link from "next/link";
 
-export default NotFound;
+export default function NotFound() {
+    return (
+        <main className="flex flex-col justify-center items-center">
+            <h2 className="text-2xl font-bold text-red-500">Not Found</h2>
+            <p>Could not find requested resource</p>
+            <Link
+                href="/"
+                className="underline underline-offset-4"
+            >
+                Return Home
+            </Link>
+        </main>
+    );
+}
