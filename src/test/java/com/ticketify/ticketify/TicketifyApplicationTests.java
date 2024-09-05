fd64459dba5d0f5c0c0e2d2b7de9aa9614ab2e45
pratikstemkar <pratikstemkar@gmail.com>
2024-09-05 15:21:34 +0530

    spring initializr init

---
+++ src/test/java/com/ticketify/ticketify/TicketifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ticketify.ticketify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
